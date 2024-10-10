c945d04f4ed432e3143ed3b4a123d2a77cd47f4d
Lydia Bandy <LydiaBandy@gmail.com>
2024-10-10 15:03:57 -0400

    config tailwind and set up add page

--- solo-project/tailwind.config.js
+++ solo-project/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // Adjust the path if using Next.js
+    "./app/**/*.{js,ts,jsx,tsx}", // Scan the 'app' directory
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
