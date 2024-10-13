1fdb7fbb7956d06995ab1b7d226f581ee01c66f9
Lydia Bandy <LydiaBandy@gmail.com>
2024-10-13 14:33:30 -0400

    successfully created signup api

--- solo-project/app/api/auth/signup/route.ts
+++ solo-project/app/api/auth/signup/route.ts
@@ -3,12 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 
 export async function POST(req: NextRequest) {
-  const { email, password } = await req.json();
-
-  // Hash the password before storing it
-  const hashedPassword = await bcrypt.hash(password, 10);
-
   try {
+    const { email, password } = await req.json();
+
+    // Hash the password before storing it
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     // Create the user in the database
     const user = await prisma.user.create({
       data: {
@@ -21,6 +21,7 @@ export async function POST(req: NextRequest) {
     return NextResponse.json(user, { status: 201 });
   } catch (error) {
     // Handle any errors that occur during user creation
+    console.error("Error creating user:", error);
     return NextResponse.json(
       { error: "User creation failed" },
       { status: 500 }
